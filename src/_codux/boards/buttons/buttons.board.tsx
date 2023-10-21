c0aa09493497b1fddbda3481700f3141433e4bd0
TomásJesus <tomasjesus1904@gmail.com>
2023-10-21 13:22:01 +0100

    fixed a deploy bug 2

--- src/_codux/boards/buttons/buttons.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import Buttons from '../../../components/Buttons';
-
-export default createBoard({
-    name: 'Buttons',
-    Board: () => <Buttons />,
-    isSnippet: true,
-});
